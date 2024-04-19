ba0d0471bc6c593febdfe1d6a0691375fe1d3827
@daaronr <8229168+daaronr@users.noreply.github.com>
2024-04-19 10:40:42 -0400

    take 3 -- Just pushing my 'quarto setup' as I work on outlining our process
    hopefully it doesn't think its a submodule this time

---
+++ writeup_embedding_code_quarto/main.R
@@ -0,0 +1,9 @@
+# one R file to do everything ####
+
+# Setup ####
+#bring in data, packages, etc, whatever else you want to do here
+# NO -- this doesn't work because it the environment is not reused across chapters!
+
+# render the book ####
+system("quarto render")
+  
